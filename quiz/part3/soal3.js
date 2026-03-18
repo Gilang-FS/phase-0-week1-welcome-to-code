643a852b2efa4d5b68aea29bc90575f5729e28c7
Gilang-FS <gilangprayudha160@gmail.com>
2026-03-18 21:30:08 +0700

    soal 3

---
+++ quiz/part3/soal3.js
@@ -0,0 +1,15 @@
+console.log("GANAP");
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
+
+console.log("\nGANJIL");
+
+for (let i = 1; i <= 100; i++) {
+  if (i % 2 == 1) {
+    console.log(i);
+  }
+}
